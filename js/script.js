af7ba577ff7d8349da99515e115efdb680395601
Kusum Ijari <kusumijari@Kusums-MacBook-Pro.local>
2017-10-11 01:53:56 -0700

    More changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-// $(document).ready(function () {
-//     $(".expandable").click(function () {
-//         console.log("Clicked");
-//         $(this).find('.toggle_visibility').toggle();
-//     });
-// }); 
-
 $(".expandable").click(function () {
-    console.log("Clicked");
     $header = $(this);
     $content = $header.next();
-    $content.slideToggle(500);
+    $content.slideToggle(750);
 });
