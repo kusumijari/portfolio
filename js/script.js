afea209dde164bf41eedb9084577c15ec61a13d4
Kusum Ijari <kusumijari@gmail.com>
2018-02-13 16:48:45 -0700

    Ability to zoom into images

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ $(".expandable").click(function () {
     $content.slideToggle(750);
 });
 
+(function(window, document){
+    window.onload = init;
+    function init () {
+		// Get the modal
+	    var modal = document.getElementById('modal');
+
+	    // Get the image and insert it inside the modal - use its "alt" text as a caption
+	    var modalImg = document.getElementById("img01");
+	    var captionText = document.getElementById("caption");
+
+	    $('img.modalImage').click(function(){
+	        modal.style.display = "block";
+	        modalImg.src = this.src;
+	        captionText.innerHTML = this.alt;
+	    });
+
+	    // Get the <span> element that closes the modal
+	    var span = document.getElementsByClassName("close")[0];
+
+	    // When the user clicks on <span> (x), close the modal
+	    span.onclick = function() { 
+	      modal.style.display = "none";
+	    }
+    }   
+})(window, document);
